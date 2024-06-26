bf2ba7f4ebe56b0309990142434bf30841debe59
K-Njio <302767830@student.rocmondriaan.nl>
2024-06-26 19:49:21 +0200

    scroll bar fixed

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,15 @@
-window.addEventListener('scroll', () => {
-    const myFunction = () => {
-      const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-      const scrolled = (winScroll / height) * 100;
-      document.getElementById('myBar').style.width = `${scrolled}%`;
-    };
-  
-    myFunction();
-  });
+// Het zet de bar op 0%
+document.getElementById('myBar').style.width = '0%';
+
+// Het is een functie dat de bar update om te kijken hoe ver het is met scrollen.
+const updateProgressBar = () => {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById('myBar').style.width = `${scrolled}%`;
+};
+
+// Voegt een scroll event aan de update functie om de proces te laten zien
+window.addEventListener('scroll', updateProgressBar);
+
+
